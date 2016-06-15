0cfa8f6732dadbff808c55fb10e60042ef4fa0d7
MatheusZandona <zan_matheus@yahoo.com.br>
2016-06-15 14:54:17 -0300

    Primeiro commit

---
+++ src/main/java/br/univel/Trabalho2BimestreProjetoVendas/App.java
@@ -0,0 +1,13 @@
+package br.univel.Trabalho2BimestreProjetoVendas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
